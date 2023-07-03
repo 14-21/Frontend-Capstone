5dca81044c4c7e3767beb09f8c0294285ebf2cd3
Colton Skiera <cskiera142@gmail.com>
2023-07-03 11:03:59 -0600

    nav updates-having trouble with default picture

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,8 @@ import { useContext } from "react";
 import { LoginContext } from "../App";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import {
-  FaArrowAltCircleDown,
-  FaArrowAltCircleUp,
-  FaArrowUp,
-  FaBaby,
-  FaItalic,
-  FaLongArrowAltUp,
-  FaRegArrowAltCircleUp,
-  FaThinkPeaks,
-} from "react-icons/fa";
+import { FaRegArrowAltCircleUp } from "react-icons/fa";
+import ProfileNav from "./ProfileNav";
 
 function Navbar() {
   const { setIsLoggedIn } = useContext(LoginContext);
@@ -31,53 +23,56 @@ function Navbar() {
           <p className="website-name-sub">Gaming Reviews</p>
         </h1>
 
-        <nav>
-          {/* <FaBars /> */}
-          {isLoggedIn ? (
-            <div className="topnav-link-bar">
-              {/* // This link is only showed when user is logged in. */}
-              <Link className="top-nav links" to="/">
-                Home
-              </Link>
-              <Link className="links" to="/profile">
-                My Profile
-              </Link>
-              <Link className="links" to="/games">
-                All Games
-              </Link>
-              <button
-                id="logout-button"
-                onClick={() => {
-                  setIsLoggedIn(false);
-                  localStorage.removeItem("token"); //Removes token from local storage when logout is clicked.
-                  localStorage.removeItem("user"); //Removes user from local storage when logout is clicked.
-                  navigate("/");
-                }}
-              >
-                Logout
-              </button>
-            </div>
-          ) : (
-            <div className="topnav-link-bar">
-              {/* // These links are showed when the user is logged out. */}
-              <Link className="links" to="/">
-                Home
-              </Link>
+        <div id="nav-profile-split">
+          <nav>
+            {/* <FaBars /> */}
+            {isLoggedIn ? (
+              <div className="topnav-link-bar">
+                {/* // This link is only showed when user is logged in. */}
+                <Link className="top-nav links" to="/">
+                  Home
+                </Link>
+                <Link className="links" to="/profile">
+                  My Profile
+                </Link>
+                <Link className="links" to="/games">
+                  All Games
+                </Link>
+                <button
+                  id="logout-button"
+                  onClick={() => {
+                    setIsLoggedIn(false);
+                    localStorage.removeItem("token"); //Removes token from local storage when logout is clicked.
+                    localStorage.removeItem("user"); //Removes user from local storage when logout is clicked.
+                    navigate("/");
+                  }}
+                >
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <div className="topnav-link-bar">
+                {/* // These links are showed when the user is logged out. */}
+                <Link className="links" to="/">
+                  Home
+                </Link>
 
-              <Link className="links" to="/games">
-                All Games
-              </Link>
+                <Link className="links" to="/games">
+                  All Games
+                </Link>
 
-              <Link className="links" to="/login">
-                Login
-              </Link>
+                <Link className="links" to="/login">
+                  Login
+                </Link>
 
-              <Link className="links" to="/register">
-                Register
-              </Link>
-            </div>
-          )}
-        </nav>
+                <Link className="links" to="/register">
+                  Register
+                </Link>
+              </div>
+            )}
+          </nav>
+          <ProfileNav />
+        </div>
       </div>
     </>
   );
