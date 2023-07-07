5b3eb8647d33e956af395682553c7364462f4e3b
Colton Skiera <cskiera142@gmail.com>
2023-07-07 08:57:50 -0600

    cleaned up some css across the project

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,69 +1,70 @@
-import './login.css';
-import { loginUser } from "../api-routes"
+import "./register.css";
+import { loginUser } from "../api-routes";
 import { useState, useContext } from "react";
-import { LoginContext } from '../App';
-import { useNavigate } from 'react-router-dom';
+import { LoginContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const {setIsLoggedIn} = useContext(LoginContext);
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setIsLoggedIn } = useContext(LoginContext);
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(username);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(username);
 
-        try {
-            const result = await loginUser(username, password);
-            console.log(result) 
+    try {
+      const result = await loginUser(username, password);
+      console.log(result);
 
-            // Fetching only key-value pair for the token for the login.
-            localStorage.setItem("token", result.token); // Potentially needs to be changed.
-            localStorage.setItem("id", result.id) // Can be deleted later - for testing purposes.
-            
-            setIsLoggedIn(true)  // Telling program login is true.
+      // Fetching only key-value pair for the token for the login.
+      localStorage.setItem("token", result.token); // Potentially needs to be changed.
+      localStorage.setItem("id", result.id); // Can be deleted later - for testing purposes.
 
-            navigate('/') //Navigates back to Homepage after login.
-        } catch (error) {
-            console.log(error);
-        }
+      setIsLoggedIn(true); // Telling program login is true.
+
+      navigate("/"); //Navigates back to Homepage after login.
+    } catch (error) {
+      console.log(error);
     }
-    
+  };
 
+  return (
+    <div id="login-container">
+      <h1 id="registerheader">LOGIN</h1>
+      <form id="registerform" onSubmit={handleSubmit}>
+        <label className="labels">
+          <input
+            className="register-inputs"
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setUsername(e.target.value);
+            }}
+          />
+        </label>
 
-    return (
-        <div id="login-container">
-        <h1 id="loginheader">LOGIN</h1>
-        <form id="loginform" onSubmit={handleSubmit}>
-          <label className="labels">
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => {
-                console.log(e.target.value);
-                setUsername(e.target.value);
-              }}
-            />
-          </label>
-  
-          <label className="labels">
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => {
-                console.log(e.target.value);
-                setPassword(e.target.value);
-              }}
-            />
-          </label>
-          <button id="loginbutton" type="submit">Submit</button>
-        </form>
-      </div>
-    )
-
+        <label className="labels">
+          <input
+            className="register-inputs"
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <button id="registerbutton" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
