be898eb105cff6e12145bdbd8a2c96a69b301abd
Colton Skiera <cskiera142@gmail.com>
2023-07-14 12:07:27 -0600

    Merge conflict

--- src/components/DeleteReviewButton.jsx
+++ src/components/DeleteReviewButton.jsx
@@ -5,15 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const BASE_URL = "http://localhost:8080";
 
-
 function DeleteReviewButton(props) {
-  const id = props.id
+  const id = props.id;
   // console.log(props)
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const result = await deleteReview(id);
-      console.log(result)
+      console.log(result);
     } catch (error) {
       console.log(error);
     }
@@ -21,41 +20,42 @@ function DeleteReviewButton(props) {
 
   async function deleteReview(reviewId) {
     try {
-        const token = localStorage.getItem("token");
-        console.log(token)
-        console.log(reviewId)
-        const response = await fetch(`${BASE_URL}/api/games/user/review/delete/${reviewId}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': `Bearer ${token}`
-            },
-
-        });  // Outside of fetch starting here.
-        const result = await response.json()
-        console.log(result)
-        if(result.success === true) {
-            const deletedFilteredReview = props.filteredReview.filter((singleReview) => {
-                if(singleReview.reviewId !== reviewId){
-                    return singleReview
-                }
-            })
-
-            props.setFilteredReview(deletedFilteredReview)
+      const token = localStorage.getItem("token");
+      console.log(token);
+      console.log(reviewId);
+      const response = await fetch(
+        `${BASE_URL}/api/games/user/review/delete/${reviewId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
         }
+      ); // Outside of fetch starting here.
+      const result = await response.json();
+      console.log(result);
+      if (result.success === true) {
+        const deletedFilteredReview = props.filteredReview.filter(
+          (singleReview) => {
+            if (singleReview.reviewId !== reviewId) {
+              return singleReview;
+            }
+          }
+        );
+
+        props.setFilteredReview(deletedFilteredReview);
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  }
 
   return (
     <div>
-       
-
-        <button onClick={handleSubmit} className="review-field-buttons">
-          Delete <FontAwesomeIcon icon={faArrowRight} size="1x" />
-        </button> 
-      
+      <button onClick={handleSubmit} className="review-field-buttons">
+        Delete <FontAwesomeIcon icon={faArrowRight} size="1x" />
+      </button>
     </div>
   );
 }
