1c4b21bfd9c81ea92bca9dcd2a27bf65d4b82955
Colton <110861475+Cskiera142@users.noreply.github.com>
2023-07-15 18:48:45 -0600

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -9,7 +9,6 @@ import CreateReviewButton from "./CreateReviewButton";
 
 function Reviews(props) {
   const [review, setReview] = useState([]);
-  const [user, setUser] = useState([])
   const [filteredReview, setFilteredReview] = useState([]);
 
   const { id } = useParams();
