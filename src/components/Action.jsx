9df1d513564ae3033ec6bca0fee7efa4d337a947
Colton Skiera <cskiera142@gmail.com>
2023-07-19 11:58:39 -0600

    change to genre cards

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,34 +1,31 @@
 import { Link } from "react-router-dom";
 import "./genrePages.css";
 
-
 function Action(props) {
-    return(
-        <section id="genre-mainpage">
-         <br/>
-         <br/>
-         <h1 id="genre-pages-header">Action</h1>
-         <br/>
-         <br/>
-            <div id="genre-subcontainer">
-                {props.allGames.length ? (
-                    props.allGames.map((e) => {
-                        if(e.genre == "Action") {
-                            return(
-                                <div key={e.gameId} className="gamecard">
-                                     <Link to={`/games/${e.gameId}`} >
-                                        <img className="genre-pic" src={e.picturecard} />
-                                        <p className="genre-game-title">{e.title}</p>
-                                    </Link>
-                                </div>
-                            )
-                            }
-                        
-                    })
-                    
-                ) : <p>Loading</p>
-
+  return (
+    <section id="genre-mainpage">
+      <br />
+      <br />
+      <h1 id="genre-pages-header">Action</h1>
+      <br />
+      <br />
+      <div id="genre-subcontainer">
+        {props.allGames.length ? (
+          props.allGames.map((e) => {
+            if (e.genre == "Action") {
+              return (
+                <div key={e.gameId} className="gamecard">
+                  <Link to={`/games/${e.gameId}`}>
+                    <img className="genre-pic" src={e.picturecard} />
+                    <p className="genre-game-title genre-dark">{e.title}</p>
+                  </Link>
+                </div>
+              );
             }
+          })
+        ) : (
+          <p>Loading</p>
+        )}
       </div>
       <br />
       <br />
--- src/components/Adventure.jsx
+++ src/components/Adventure.jsx
@@ -1,36 +1,31 @@
 import "./genrePages.css";
 import { Link } from "react-router-dom";
 
-
 function Adventure(props) {
-    return(
-        <section id="genre-mainpage">
-         <br/>
-         <br/>
-         <h1 id="genre-pages-header">Adventure</h1>
-         <br/>
-         <br/>
-            <div id="genre-subcontainer">
-                {props.allGames.length ? (
-                    props.allGames.map((e) => {
-                        if(e.genre == "Adventure") {
-                            return(
-
-                                <div key={e.gameId} className="gamecard">
-                                    <Link to={`/games/${e.gameId}`} >
-                                        <img className="genre-pic" src={e.picturecard} />
-                                        <p className="genre-game-title">{e.title}</p>
-                                    </Link>
-
-                                </div>
-                            )
-                            }
-                        
-                    })
-                    
-                ) : <p>Loading</p>
-
+  return (
+    <section id="genre-mainpage">
+      <br />
+      <br />
+      <h1 id="genre-pages-header">Adventure</h1>
+      <br />
+      <br />
+      <div id="genre-subcontainer">
+        {props.allGames.length ? (
+          props.allGames.map((e) => {
+            if (e.genre == "Adventure") {
+              return (
+                <div key={e.gameId} className="gamecard">
+                  <Link to={`/games/${e.gameId}`}>
+                    <img className="genre-pic" src={e.picturecard} />
+                    <p className="genre-game-title genre-dark">{e.title}</p>
+                  </Link>
+                </div>
+              );
             }
+          })
+        ) : (
+          <p>Loading</p>
+        )}
       </div>
       <br />
       <br />
@@ -41,4 +36,3 @@ function Adventure(props) {
 }
 
 export default Adventure;
-
